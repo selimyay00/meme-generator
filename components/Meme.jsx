c3b9afbe0acfefd9bcf01a513e0b4e0d0de3b47a
selim yay <selimxyay@gmail.com>
2023-11-13 22:43:23 +0300

    connected the inputs to proper state variables

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,17 +1,54 @@
+import { useState } from "react"
+
 function Meme() {
+    // Top text, bottom text and image will change
+      // I have to track them and update the UI
+        // That's why I need to use the useState Hook
+
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        imageUrl: ''
+    })
+    
+    function handleClick() {
+        alert("button clicked!")
+    }
+
+    function handleChange(event) {
+        const name = event.target.name
+        const value = event.target.value
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        })) 
+    }
+    
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <div className="form-inputs">
-                    <input type="text" placeholder="Top text" />
-                    <input type="text" placeholder="Bottom text" />
+                    {/* TODO: Connect the inputs to proper states */}
+                    <input 
+                        type="text" 
+                        placeholder="Top text"
+                        value={meme.topText}
+                        onChange={handleChange}  
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="Bottom text"
+                        value={meme.bottomText} 
+                        onChange={handleChange}
+                    />
                 </div>
                 <button
                     className="form-button"
+                    onClick={handleClick}
                 >
                     Get a new meme image
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
