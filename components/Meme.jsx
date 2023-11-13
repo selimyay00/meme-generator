635964465e22173e7a3d0148c7810dc8068aa2c6
selim yay <selimxyay@gmail.com>
2023-11-13 15:29:01 +0300

    prepared the body of meme generator

---
+++ components/Meme.jsx
@@ -0,0 +1,13 @@
+function Meme() {
+    return (
+        <main>
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <button>Get a new meme image</button>
+            </form>
+        </main>
+    )
+}
+
+export default Meme
